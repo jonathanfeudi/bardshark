b6d0d8e36389a01b1bd0a21c964326c71141c929
Jonathan Feudi <jonathanfeudi@gmail.com>
2017-10-31 17:30:29 -0400

    Server is running

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const path = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+//const db = require('./db/pgp');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//load .env
+dotenv.load();
+
+//create static route to public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//log (morgan)
+app.use(logger('dev'));
+
+//looks for '_method' in the post request from a form, then changes method to DELETE
+app.use(methodOverride('_method'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+  res.render('pages/home')
+});
+
+app.listen(port,()=>
+  console.log('Server online - Shark available for Tasting on ',port,'//', new Date())
+);
