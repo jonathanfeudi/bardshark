c5a48fbb39bc244bbc257a23cec7079dd56cc8a7
Jonathan Feudi <jonathanfeudi@gmail.com>
2018-06-01 18:36:48 -0400

    Still trying

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,19 @@ users.post('/', db.createSubscriber, function(req, res){
   res.redirect('/');
 });
 
+/*
 users.post('/admin', db.createUser, function(req, res){
   res.redirect('/');
 });
+*/
 
+/*
 users.post('/login', db.loginUser, function(req, res){
   req.session.user = res.rows;
   req.session.save(function(){
     res.redirect('/')
   });
 });
+*/
 
 module.exports = users;
